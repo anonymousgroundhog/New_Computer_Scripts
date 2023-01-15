361ee830c6b527651f783f9cae3fe57f06bc4cd0
anonymousgroundhog <leapfrogopus@gmail.com>
2023-01-15 16:12:00 -0500

    Adding install files script

---
+++ Install_Apps.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+sudo apt-add-repository ppa:maarten-fonville/android-studio
+sudo apt install snapd
+sudo apt update
+sudo apt-get install android-studio -y
+sudo snap install skype --classic 
+sudo snap install nvim --classic
+sudo apt-get install discord -y
+sudo apt-get install ticktick
